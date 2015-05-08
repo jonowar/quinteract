979d25954d149d206f2026a17024d927d2f2ef8a
Jono Warren <jono.warren@adroll.com>
2015-05-08 14:43:53 -0700

    add method to overlay rectangles on top of characters

--- quinteract/objects.py
+++ quinteract/objects.py
@@ -1,7 +1,7 @@
 import subprocess
 import tempfile
 import os
-from PIL import Image
+from PIL import Image, ImageDraw
 
 class CharacterBox(object):
     def __init__(self, value, x1, y1, x2, y2):
@@ -21,6 +21,14 @@ class CharacterBox(object):
             )
 
     @property
+    def topleft(self):
+        return (self.x1, self.y1)
+
+    @property
+    def bottomright(self):
+        return (self.x2, self.y2)
+
+    @property
     def area(self):
         return (self.x2 - self.x1) * (self.y2 - self.y1)
 
@@ -33,11 +41,14 @@ class Quinteract(object):
     TEMP_PREFIX = 'quinteract'
 
     def __init__(self, filename=None):
-        self.characters = []
         self.filename = filename
         self.process()
 
     def process(self):
+        im = Image.open(self.filename)
+        self.width, self.height = im.size
+        im.close()
+
         outputbase = os.path.join(self.TEMP_DIR, self.TEMP_PREFIX)
         try:
             subprocess.check_output(["tesseract", self.filename, outputbase, "makebox"], stderr=subprocess.STDOUT)
@@ -48,20 +59,23 @@ class Quinteract(object):
         boxfile_path = "{}.box".format(outputbase)
         textfile_path = "{}.txt".format(outputbase)
 
+        self.characters = []
         with open(boxfile_path, 'rU') as box_file:
             for i, line in enumerate(box_file.read().strip().split('\n')):
-                self.characters.append(CharacterBox(*line.split()[:5]))
+                value, x1, y1, x2, y2 = line.split()[:5]
+                # NOTE: the Image library treats (0, 0) as the top left of an image
+                # whereas tesseract .box format treats (0, 0) as the bottom left,
+                # which is why we have to flip the y-values
+                tmp1, tmp2 = int(y1), int(y2)
+                y1 = self.height - tmp2
+                y2 = self.height - tmp1
+                self.characters.append(CharacterBox(value, x1, y1, x2, y2))
 
-        import pdb; pdb.set_trace()
         os.unlink(boxfile_path)
 
         with open(textfile_path, 'rU') as text_file:
             self._text = text_file.read()
         os.unlink(textfile_path)
-
-        im = Image.open(self.filename)
-        self.width, self.height = im.size
-        im.close()
 
     @property
     def area(self):
@@ -76,5 +90,17 @@ class Quinteract(object):
         character_area = sum([c.area for c in self.characters])
         return float(character_area) / self.area
 
-    def generate_overlay(self):
-        pass
+    def generate_overlay(self, filename="overlay.png", new=False, fill=None):
+        if new:
+            im = Image.new("RGBA", (self.width, self.height), color=fill)
+        else:
+            im = Image.open(self.filename)
+
+        draw = ImageDraw.Draw(im)
+        for charbox in self.characters:
+            draw.rectangle((charbox.topleft, charbox.bottomright), fill="black")
+
+        with open(filename, 'w') as imgfile:
+            im.save(imgfile)
+
+        return filename
